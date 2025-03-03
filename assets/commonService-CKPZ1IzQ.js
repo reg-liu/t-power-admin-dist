5f4c2d0b0dfe770f8928ccaa5b51f8007a239e57
刘磊 <lei.liu@goland.cn>
2025-03-03 12:54:16 +0800

    feat: 联调

--- assets/commonService-CKPZ1IzQ.js
+++
@@ -1,1 +0,0 @@
-import{g as a}from"./index-CJS0LehZ.js";const r=()=>a.get({url:"/charge-point-brands/"}),t=o=>a.post({url:"/charge-point-brands/",data:o}),n=o=>a.post({url:"/image-upload/",data:o}),e={getBrands:r,addBrand:t,uploadImage:n};export{e as c};
