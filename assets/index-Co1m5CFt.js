793efb89178064eb8bc703f40780f3f10ed590d5
刘磊 <lei.liu@goland.cn>
2025-03-17 13:03:54 +0800

    feat: 联调

--- assets/index-Co1m5CFt.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-BgfX3fZu.js";import"./vendor-react-BcJS7s_i.js";import"./vendor-ui-DPkccqlC.js";import"./vendor-antd-B62eGpCx.js";import"./vendor-utils-CtEfXJzW.js";function f({src:l=""}){return r.jsx("div",{className:"h-full w-full",children:r.jsx("iframe",{src:l,title:"iframe-page",className:"h-full w-full"})})}export{f as default};
