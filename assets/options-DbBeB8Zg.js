aea633d962b88cda6864696d7eb585fc3ace1cfd
刘磊 <lei.liu@goland.cn>
2025-03-17 10:33:56 +0800

    feat: 联调feat: 联调

---
+++ assets/options-DbBeB8Zg.js
@@ -0,0 +1,1 @@
+const o=["AC","DC"],s=["ocpp2.0.1","ocpp1.6"],c=["1p+n","3p+n"],p=["1","2"];export{p as a,o as c,s as o,c as p};
---
+++ assets/toolbar-CzZ9CE04.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-5T_VQxhD.js";import{W as t}from"./vendor-antd-B62eGpCx.js";import"./vendor-react-BcJS7s_i.js";import"./vendor-ui-DPkccqlC.js";import"./vendor-utils-CtEfXJzW.js";function n({onRefresh:o}){return r.jsx("div",{className:"operation-area flex items-center justify-end",children:r.jsx(t,{className:"cursor-pointer text-lg",onClick:o})})}export{n as default};
