50343b6c2844a4b9511c74f505e5c3cd7f4dd52b
刘磊 <lei.liu@goland.cn>
2025-04-10 17:04:58 +0800

    feat: 联调

--- assets/index-2MUoBF22.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-D-AkrFnk.js";import"./vendor-react-DcqtUZ_G.js";import"./vendor-ui-BASYQBt4.js";import"./vendor-antd-DC1bX4vL.js";import"./vendor-utils-DUuMHeSo.js";function f({src:l=""}){return r.jsx("div",{className:"h-full w-full",children:r.jsx("iframe",{src:l,title:"iframe-page",className:"h-full w-full"})})}export{f as default};
